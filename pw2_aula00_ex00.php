d7fa3dff05667f08f5425de8d147a17abb9376a4
rogcolaco <50340398+rogcolaco@users.noreply.github.com>
2019-08-08 12:32:01 -0300

    Update pw2_aula00_ex00.php

--- pw2_aula00_ex00.php
+++ pw2_aula00_ex00.php
@@ -11,3 +11,50 @@
 	$nota['Monica'] = 8;
 	debug($nota);
 ?>
+
+<?php 
+    $vetor = array (1,2,3,4);
+    debug($vetor);
+?>
+
+<?php 
+    $professores = array (
+        "0" => array  (
+            "nome" => "Rodrigo Ramos",
+            "turma" => "651",
+            "prontuario" => "1201506",
+        ),
+        "1" => array  (
+            "nome" => "Henrique",
+            "turma" => "638",
+            "prontuario" => "0800001",
+        )
+        );
+    debug($professores);
+?>
+
+<?php 
+    $turmas = array (
+        "0" => array  (
+            "nome" => "Rodrigo Ramos",
+            "turma" => "651",
+            "prontuario" => "1201506",
+            "materias" => array (
+                "LP1" => 10,
+                "EPO" => 10,
+                "OPE" => 6,
+            )
+        ),
+        "1" => array  (
+            "nome" => "Henrique",
+            "turma" => "638",
+            "prontuario" => "0800001",
+            "materias" => array (
+                "LP1" => 2,
+                "EPO" => 0,
+                "OPE" => 8,
+            )
+        )
+        );
+    debug($turmas);
+?>
