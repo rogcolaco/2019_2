289a5e96230f5c63cc6c049939eddab1e451333b
rogcolaco <50340398+rogcolaco@users.noreply.github.com>
2019-09-03 11:31:02 -0300

    Update Ead01.php

--- Ead01.php
+++ Ead01.php
@@ -13,6 +13,33 @@
             echo "</pre>";
          }
 
+         $caminhoArquivo = "./arquivos/departamento.csv";
+         $arquivo = fopen( $caminhoArquivo, "r" );
+         
+         if( $arquivo == false ) {
+            echo ("Erro ao abrir o arquivo");
+            exit();
+         }
+
+         $filesize = filesize( $caminhoArquivo );
+         echo ( "Tamanho do arquivo 2: $filesize bytes" );
+
+         $departamentos = [];
+         while (($linha = fgets($arquivo)) !== FALSE) {
+            $departamento = [];
+            $linha=str_replace('"', '', $linha);
+            $linhaExplodida = explode(';',$linha);
+            //Cria um array associativo para o funcionario atual
+            $departamento["numero"]=$linhaExplodida[0];
+            $departamento["nome"]=$linhaExplodida[1];
+            $departamento["qtdFuncionarios"]="";
+            $departamento["mediaSalarial"]="";
+            array_push($departamentos, $departamento);
+         }  
+         
+         fclose( $arquivo );
+
+
          $caminhoArquivo = "./arquivos/funcionario.csv";
          //Abre como apenas leitura ("r")
          $arquivo = fopen( $caminhoArquivo, "r" );
@@ -25,6 +52,7 @@
          }
          
          $filesize = filesize( $caminhoArquivo );
+         echo("<br>");
          echo ( "Tamanho do arquivo : $filesize bytes" );
          //$conteudoArquivo = fread( $arquivo, $filesize );
          //debug($conteudoArquivo);
@@ -48,53 +76,43 @@
             $funcionario["id"]=$linhaExplodida[0];
             $funcionario["nome"]=$linhaExplodida[1];
             $funcionario["salario"]=$linhaExplodida[6];
-            $funcionario["idDepartamento"]=$linhaExplodida[8];
+            $funcionario["idDepartamento"]=(int)$linhaExplodida[8];
             array_push($funcionarios, $funcionario);
          }
          //Fecha o arquivo csv
          fclose( $arquivo );
 
-         $caminhoArquivo = "./arquivos/departamento.csv";
-         $arquivo = fopen( $caminhoArquivo, "r" );
-         
-         if( $arquivo == false ) {
-            echo ("Erro ao abrir o arquivo");
-            exit();
-         }
-
-         $filesize = filesize( $caminhoArquivo );
-         echo("<br>");
-         echo ( "Tamanho do arquivo 2: $filesize bytes" );
-
-         $departamentos = [];
-         while (($linha = fgets($arquivo)) !== FALSE) {
-            $departamento = [];
-            $linha=str_replace('"', '', $linha);
-            $linhaExplodida = explode(';',$linha);
-            //Cria um array associativo para o funcionario atual
-            $departamento["numero"]=$linhaExplodida[0];
-            $departamento["nome"]=$linhaExplodida[1];
-            array_push($departamentos, $departamento);
-         }
-         
-         for($i = 1; $i < (count($funcionarios)); $i++){
-            $idDepartamentoFuncionario = $funcionarios[$i]["idDepartamento"];
-            for($j = 1; $j < (count($departamentos)); $j++){
-               $numeroDepartamento = $departamentos[$j]["numero"];
-               debug($idDepartamentoFuncionario);
-               debug($numeroDepartamento);
+         for($i = 0; $i < (count($funcionarios)); $i++){
+            $idDepartamentoFuncionario = (int)$funcionarios[$i]["idDepartamento"];
+            
+            for($j = 0; $j < (count($departamentos)); $j++){
+               $numeroDepartamento = (int)$departamentos[$j]["numero"];
                
                if($idDepartamentoFuncionario==$numeroDepartamento){
-                  /*debug($idDepartamentoFuncionario);
-                  debug($numeroDepartamento);
-                  debug($departamentos[$j]["nome"]);*/
+                  $funcionarios[$i]["nomeDepartamento"] = $departamentos[$j]["nome"]; 
+                  if($j == 0 ){
+                     $departamentos[$j]["qtdFuncionarios"] = "qtd Funcionarios";
+                     $departamentos[$j]["mediaSalarial"] = "media Salarial";
+                     $departamentos[$j+1]["qtdFuncionarios"]=(int) 0;
+                     $departamentos[$j+1]["mediaSalarial"] = (double) 0;
+                  } else {
+                     $departamentos[$j]["qtdFuncionarios"] = (int)$departamentos[$j]["qtdFuncionarios"] + 1;
+                     $departamentos[$j]["mediaSalarial"] = (double)$departamentos[$j]["mediaSalarial"] + $funcionarios[$i]["salario"];
+                  }
+                  break;
                }
+               unset($novoDepartamentoFuncionario);
             }
          }
-         
+
+         for($j = 1; $j < (count($departamentos)); $j++){
+            $media = $departamentos[$j]["mediaSalarial"]/$departamentos[$j]["qtdFuncionarios"];
+            $departamentos[$j]["mediaSalarial"] = round($media,2);
+         }
 
          debug($departamentos);
          debug($funcionarios);
+
 
          //Transforma em JSON e salva no arquivo.
          $funcionarios = json_encode($funcionarios);
@@ -108,6 +126,20 @@
             exit();
          }
          fwrite( $file, $funcionarios);
+         fclose( $file );
+
+         //JSON
+         $departamentos = json_encode($departamentos);
+         debug($departamentos);
+
+         $filename = "./arquivos/departamentos.json";
+         $file = fopen( $filename, "w" );
+         
+         if( $file == false ) {
+            echo ( "Erro ao abrir o arquivo" );
+            exit();
+         }
+         fwrite( $file, $departamentos);
          fclose( $file );
 
 
